08c69ba72e65132dc820f2fbb13c10a95a83eadf
De Silva Y A S <it23551084@my.sliit.lk>
2026-02-16 13:06:31 +0530

    Add collaborator names display to app.js (By De Silva Y A S- IT23551084)

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Node project started successfully!");
+
+// Displaying the collaborators' names in the console
+console.log("Collaborators: De Silva Y A S(IT23551084) & Sahan Viduranga");
